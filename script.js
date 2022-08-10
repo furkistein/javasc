92261fe27cae4c42a73b5e512d7de61e822e3a20
furkistein <furkanboztas144@gmail.com>
2022-08-10 11:47:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// alert("Hello World"); /*Sitenin yukarısında çıkan uyarıdır*/
+
+console.log("Burası console'a yazılan bir yazıdır"); /*Burası öğreyi denetle yaptığımızda çıkan console'a yazdırır*/
+
+// Aritmetik İşlemler
+
+console.log(1 + 4); //Toplama işlemidir.
+console.log(5 - 2);//Çıkartma işlemidir.
+console.log(3 * 4);//Çarpma işlemidir.
+console.log(3 % 2);//Mod almaya yarar
+console.log(3 ** 4);//Üs almaya yarar.
+
+let ismim;
+console.log(ismim); //console.log kısmına boş bir değer girdiğimiz için(let kısmında bir yazı olmadığı için yada değer vermediğimiz için konsolda "undefined" olarak gözükecektir.)
+
+let bosDeger = null; //"null" JavaScript'te boş değer anlamına gelir.
+
+console.log(typeof "Asabeneh"); //string türündendir.Çift tırnak içerisinde her türlü değeri barındırabilir.
+console.log(typeof 5); //number türündendir ve sadece sayı yazılabilir.
+console.log(true); //boolean türündendir.Bu değerin doğru yada yanlış olduğunu belirtmek için kullanılır.
+console.log(typeof null); //object type türündendir.
+console.log(undefined); //undefined türündendir.
+
+// NOT
+/*
+-JavaScript değişken adı bir sayı ile başlamamalıdır.
+-JavaScript değişken adı, dolar işareti ve alt çizgi dışında özel karakterlere izin vermez.(Türkçe karakterlere özellike dikkat ediniz.)
+-JavaScript değişken adı, camelCase kuralına uymalı.
+-JavaScript değişken adı sözcükler arasında boşluk olmamalıdır.
+*/
+
+let ad = "Furkan";
+let soyAd = "Boztas";
+let ulke = "Türkiye";
+let sehir = "Denizli";
+let yas = "18";
+let instagram = "@furkiistein";
+let evliMi = "false";
+
+console.log(ad, soyAd, ulke, sehir, yas, instagram, evliMi);
+
+
+
+
+
+
